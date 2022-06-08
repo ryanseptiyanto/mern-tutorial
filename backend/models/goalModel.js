e672ae62ea0b96da25d4c2a024ff3bee79afa349
RyanSeptiyanto <nur.septiyanto@gmail.com>
2022-06-08 10:58:04 +0700

    Membuat model dan CRUD

---
+++ backend/models/goalModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const goalSchema = mongoose.Schema({
+  text: {
+    type: String,
+    required: [true, 'Please add a text value']
+  },
+}, {
+  timeStamps: true,
+})
+
+module.exports = mongoose.model('Goals', goalSchema)
